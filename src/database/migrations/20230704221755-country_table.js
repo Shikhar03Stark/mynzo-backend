6c98e9e68ec690e7fb7b43b930c406a123ea0462
Harshit Vishwakarma (Shikhar03Stark) <hv.harshit321@gmail.com>
2023-07-05 04:05:59 +0530

    db migrations

---
+++ src/database/migrations/20230704221755-country_table.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  UUIDV4,
+  STRING,
+  UUID
+} = require('sequelize');
+const {
+  DATE
+} = require('sequelize/lib/data-types');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('country', {
+      id: {
+        type: UUID,
+        defaultValue: UUIDV4,
+        primaryKey: true,
+      },
+      value: {
+        type: STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    }, );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('country');
+  }
+};
---
+++ src/database/migrations/20230704222536-city_table.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const {
+  STRING,
+  UUID,
+  UUIDV4,
+  DATE
+} = require('sequelize/lib/data-types');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('city', {
+      id: {
+        type: UUID,
+        defaultValue: UUIDV4,
+        primaryKey: true,
+      },
+      value: {
+        type: STRING,
+        allowNull: false,
+      },
+      state_id: {
+        type: STRING,
+        allowNull: true,
+      },
+      created_at: {
+        type: DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DATE,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('city');
+  }
+};
